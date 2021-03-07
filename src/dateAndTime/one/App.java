0599dd3e20dcae11c3cc8c5fd4cb9f26bcffd22b
inpyeong <jinipyung@gmail.com>
2021-03-07 14:23:20 +0900

    Date와 Time 소개

--- src/dateAndTime/one/App.java
+++ src/dateAndTime/one/App.java
@@ -1,2 +1,21 @@
-package dateAndTime.one;public class App {
+package dateAndTime.one;
+
+import java.util.Date;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        Date date = new Date();
+        long time = date.getTime();
+        System.out.println(date);
+        System.out.println(time);
+
+        // java.util.Date 클래스는 mutable 하다.
+        // thread safe 하지 않다.
+        Thread.sleep(1000 * 3);
+        Date after3Seconds = new Date();
+        System.out.println(after3Seconds);
+        after3Seconds.setTime(time);
+        System.out.println(after3Seconds);
+    }
 }
