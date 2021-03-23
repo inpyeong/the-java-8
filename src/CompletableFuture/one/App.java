194318fce107655dc97fe6ab7c90c9475d1db7e3
inpyeong <jinipyung@gmail.com>
2021-03-23 09:57:20 +0900

    자바 Concurrent 프로그래밍 소개

--- src/CompletableFuture/one/App.java
+++ src/CompletableFuture/one/App.java
@@ -1,2 +1,47 @@
-package CompletableFuture.one;public class App {
+package CompletableFuture.one;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        System.out.println("Hello " + Thread.currentThread().getName());
+
+        Thread thread = new Thread(() -> {
+//            while(true) {
+//                System.out.println(Thread.currentThread().getName());
+//
+//                try {
+//                    Thread.sleep(1000L);
+//                } catch (InterruptedException e) {
+//                    System.out.println("Exit!");
+////                    e.printStackTrace();
+////                    리턴하지 않으면 계속 진행된다.
+////                    return;
+//                }
+//
+//            }
+            System.out.println("Thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+        thread.start();
+
+        System.out.println("Hello " + Thread.currentThread().getName());
+//        Thread.sleep(3000L);
+        thread.join();
+//        thread.interrupt();
+        System.out.println(thread + " is finished");
+    }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Thread: " + Thread.currentThread().getName());
+        }
+    }
 }
